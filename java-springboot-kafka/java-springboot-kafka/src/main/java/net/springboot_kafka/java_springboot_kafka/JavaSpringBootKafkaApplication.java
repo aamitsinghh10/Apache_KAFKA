23b0e103d3f40aeaec59e4166561039743f1082a
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-07-19 19:51:01 +0530

    Add files via upload

---
+++ java-springboot-kafka/java-springboot-kafka/src/main/java/net/springboot_kafka/java_springboot_kafka/JavaSpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package net.springboot_kafka.java_springboot_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootKafkaApplication.class, args);
+	}
+
+}
---
+++ java-springboot-kafka/java-springboot-kafka/src/test/java/net/springboot_kafka/java_springboot_kafka/JavaSpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot_kafka.java_springboot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
