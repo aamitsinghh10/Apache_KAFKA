7f2f26451d40d4e123ceee6d819606fe33b2b002
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-07-19 20:29:05 +0530

    Add files via upload

---
+++ java-springboot-kafka/java-springboot-kafka/src/main/java/net/springboot_kafka/java_springboot_kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,15 @@
+package net.springboot_kafka.java_springboot_kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic KafkaTopics(){
+        //return TopicBuilder.name("Kafka-Topics").partitions(10).build();
+        return TopicBuilder.name("Kafka-Topics").build();//here using by default partitions
+    }
+}
